afb2484cc83c01d4a8d894d3dfd28c586e34db52
Sudar278 <119469584+Sudar278@users.noreply.github.com>
2024-02-25 17:18:33 +0530

    Add files via upload

---
+++ serve_app.py
@@ -0,0 +1,48 @@
+import os 
+import streamlit as st
+from llama_index.core import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms.openai import OpenAI
+import openai
+from llama_index.core import SimpleDirectoryReader
+
+st.set_page_config(page_title="Paperflite Chatbot", layout="centered", initial_sidebar_state="auto", menu_items=None)
+os.environ["OPENAI_API_KEY"] = "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"
+openai.api_key = os.environ["OPENAI_API_KEY"]
+st.title("Chat with Paperflitebot")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Paperflite "}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Paperflite chatbot"):
+        reader = SimpleDirectoryReader(input_dir="FAQ", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.1, 
+                            system_prompt="""You are inteligent customer chatbot of paperflite.
+                            Try to solve customer queries.Donot try to make up answer"""))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
